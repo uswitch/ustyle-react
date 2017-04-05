e566625e7b6911e5867ab0fbc65d0b931109055f
Jack Stevens <jpstevens@users.noreply.github.com>
2017-04-05 16:34:42 +0100

    Extract .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-2']
-        }
+        loader: 'babel-loader'
       }
     ]
   }
