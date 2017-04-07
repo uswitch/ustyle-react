858fdd59a199353fcd3f9e28be6760759cbd180d
Jack Stevens <jpstevens@users.noreply.github.com>
2017-04-07 17:19:05 +0100

    Bump yarn deps

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,19 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Loader = exports.USP = exports.ProgressNavigation = exports.Breadcrumbs = exports.Button = undefined;
+exports.USP = exports.ProgressNavigation = exports.Loader = exports.Button = exports.Breadcrumbs = undefined;
+
+var _Breadcrumbs = require('./components/Breadcrumbs');
+
+var _Breadcrumbs2 = _interopRequireDefault(_Breadcrumbs);
 
 var _Button = require('./components/Button');
 
 var _Button2 = _interopRequireDefault(_Button);
 
-var _Breadcrumbs = require('./components/Breadcrumbs');
+var _Loader = require('./components/Loader');
 
-var _Breadcrumbs2 = _interopRequireDefault(_Breadcrumbs);
+var _Loader2 = _interopRequireDefault(_Loader);
 
 var _ProgressNavigation = require('./components/ProgressNavigation');
 
@@ -21,14 +25,10 @@ var _USP = require('./components/USP');
 
 var _USP2 = _interopRequireDefault(_USP);
 
-var _Loader = require('./components/Loader');
-
-var _Loader2 = _interopRequireDefault(_Loader);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.Button = _Button2.default;
 exports.Breadcrumbs = _Breadcrumbs2.default;
+exports.Button = _Button2.default;
+exports.Loader = _Loader2.default;
 exports.ProgressNavigation = _ProgressNavigation2.default;
 exports.USP = _USP2.default;
-exports.Loader = _Loader2.default;
