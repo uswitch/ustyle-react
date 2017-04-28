3bc97795c741f26b2db62a17fe719389cbd1191f
Thomas Britton <tompbritton@gmail.com>
2017-04-28 18:09:44 +0100

    Add a checkbox and radio input component #12

---
+++ src/components/Radio/index.js
@@ -0,0 +1,30 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { omit } from 'lodash';
+
+export default class Radio extends PureComponent {
+  get className () {
+    return cx({
+      "us-form-input": true,
+      "us-form-input--disabled": this.props.disabled,
+    })
+  }
+
+  render () {
+    const { onChange, checked, children, name, ...other} = this.props;
+    const newProps = omit(this.props, 'type', 'children', 'className', 'onChange');
+
+    return (
+      <label>
+        <input {...newProps} checked={checked} className={this.className} name={name} type="radio" onChange={onChange} />
+        {children}
+      </label>
+    )
+  }
+}
+
+Radio.propTypes = {
+  name: PropTypes.string,
+  onChange: PropTypes.func.isRequired
+}
