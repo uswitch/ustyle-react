b55c9d249f15a214297c0f1c4143f2f300e7685b
Dom Charlesworth <dgc336@gmail.com>
2017-10-12 11:21:58 +0100

    Add script to auto-generate `colors.js` module of ustyle colors

---
+++ scripts/generate-ustyle-colors.js
@@ -0,0 +1,34 @@
+const { promisify } = require('util')
+const { writeFileSync } = require('fs')
+const { resolve } = require('path')
+
+const sass = require('node-sass')
+const sassExtract = require('sass-extract')
+
+const render = promisify(sass.render)
+const extract = sassExtract.extract
+
+const file = require.resolve('ustyle/vendor/assets/stylesheets/ustyle/basics/variables/_colors.scss')
+const sLowerCamelCase = (s) => s
+      .split(/[-_]/)
+      .reduce((acc, [ first, ...rest ], i) => `${acc}${i === 0 ? first : first.toUpperCase()}${rest.join('')}`, '')
+
+const generatedComment = `/*
+  This file is auto generated at build time, do not edit!
+*/
+`
+
+render({ file })
+  .then(extract)
+  .then(({ global }) => {
+    const colors = Object
+          .entries(global)
+          .map(([key, { value }]) => ([ [sLowerCamelCase(key).slice(1)], value.hex ]))
+          .reduce((acc, [ key, val ]) => acc.concat(`export const ${key} = '${val}'`), [])
+          .join('\n')
+
+    writeFileSync(
+      resolve(__dirname, '../src/colors.js'),
+      `${generatedComment}${colors}`
+    )
+  })
